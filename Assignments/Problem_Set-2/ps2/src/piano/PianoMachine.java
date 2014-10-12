6003b229ff1da0f62122eaa2cf1e72575cb0ec27
Lawrence R. Amlord <informlarry@gmail.com>
2014-10-12 23:38:06 +0800

    add solution to Problem 4 in Problem Set 2.
    Augmented are the following 2 files:
    
    	piano/PianoMachine.java
    	piano/PianoMachineTest.java
    
    Thank you!
    							Lawrence X. Amlord
    							  Oct 12th, 2014
    							   Xi'an, China

--- Assignments/Problem_Set-2/ps2/src/piano/PianoMachine.java
+++ Assignments/Problem_Set-2/ps2/src/piano/PianoMachine.java
@@ -1,8 +1,14 @@
 package piano;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 import javax.sound.midi.MidiUnavailableException;
 
 import midi.Midi;
+import music.NoteEvent;
 import music.Pitch;
 import music.PitchLevel;
 
@@ -11,7 +17,10 @@ public class PianoMachine {
 	private Midi midi;
 	private midi.Instrument instrument = Midi.DEFAULT_INSTRUMENT;
 	
+	// level of pitch (in octaves) compared to the starting note pitch
 	private PitchLevel pitchLevel = PitchLevel.DEFAULT_LEVEL;
+	public boolean isRecording = false;
+	public List<NoteEvent> recordedEvents = new ArrayList<NoteEvent>();
 	
 	/**
 	 * constructor for PianoMachine.
@@ -34,26 +43,14 @@ public class PianoMachine {
      * 					be among the set {0, 1, 2, ..., 10, 11}
      */
     public void beginNote(Pitch rawPitch) {
-    	Pitch modulatedPitch = rawPitch;
-    	switch (pitchLevel) {
-    	case DEFAULT_LEVEL:
-    		modulatedPitch = rawPitch;
-    		break;
-    	case ONE_OCTAVE_BELOW:
-    		modulatedPitch = rawPitch.transpose((Pitch.OCTAVE) * (-1));
-    		break;
-    	case TWO_OCTAVES_BELOW:
-    		modulatedPitch =
-    			rawPitch.transpose((Pitch.OCTAVE) * (-1)).transpose((Pitch.OCTAVE) * (-1));
-    		break;
-    	case ONE_OCTAVE_ABOVE:
-    		modulatedPitch = rawPitch.transpose(Pitch.OCTAVE);
-    		break;
-    	case TWO_OCTAVES_ABOVE:
-    		modulatedPitch =
-    			rawPitch.transpose(Pitch.OCTAVE).transpose(Pitch.OCTAVE);
-    		break;
+    	long time = getCurrentTime();
+    	NoteEvent event;
+    	event = new NoteEvent(rawPitch, time,
+    						  instrument, NoteEvent.Kind.start);
+    	if (isRecording) {
+    		recordedEvents.add(event);
     	}
+    	Pitch modulatedPitch = modulatePitch(rawPitch);
     	midi.beginNote(modulatedPitch.toMidiFrequency(), instrument);
     }
     
@@ -63,26 +60,14 @@ public class PianoMachine {
      * 					be among the set {0, 1, 2, ..., 10, 11}
      */
     public void endNote(Pitch rawPitch) {
-    	Pitch modulatedPitch = rawPitch;
-    	switch (pitchLevel) {
-    	case DEFAULT_LEVEL:
-    		modulatedPitch = rawPitch;
-    		break;
-    	case ONE_OCTAVE_BELOW:
-    		modulatedPitch = rawPitch.transpose((Pitch.OCTAVE) * (-1));
-    		break;
-    	case TWO_OCTAVES_BELOW:
-    		modulatedPitch =
-    			rawPitch.transpose((Pitch.OCTAVE) * (-1)).transpose((Pitch.OCTAVE) * (-1));
-    		break;
-    	case ONE_OCTAVE_ABOVE:
-    		modulatedPitch = rawPitch.transpose(Pitch.OCTAVE);
-    		break;
-    	case TWO_OCTAVES_ABOVE:
-    		modulatedPitch =
-    			rawPitch.transpose(Pitch.OCTAVE).transpose(Pitch.OCTAVE);
-    		break;
+    	long time = getCurrentTime();
+    	NoteEvent event;
+    	event = new NoteEvent(rawPitch, time,
+    						  instrument, NoteEvent.Kind.stop);
+    	if (isRecording) {
+    		recordedEvents.add(event);
     	}
+    	Pitch modulatedPitch = modulatePitch(rawPitch);
     	midi.endNote(modulatedPitch.toMidiFrequency(), instrument);
     }
     
@@ -146,15 +131,110 @@ public class PianoMachine {
     	}
     }
     
-    //TODO write method spec
+    /**
+     * Toggle between recording on and off. The previous
+     * recording will be overridden, if a new recording is
+     * made.
+     * @return true if the piano machine begins recording, or
+     * 		   false if the piano machine ends recording.
+     */
     public boolean toggleRecording() {
-    	return false;
-    	//TODO: implement for question 4
+    	if (isRecording) {	// the piano machine is recording before, terminate recording
+    		isRecording = false;
+    	} else {	// the piano machine is not recording before, begin recording
+    		isRecording = true;
+    		recordedEvents = new ArrayList<NoteEvent>();
+    	}
+    	return isRecording;
     }
     
-    //TODO write method spec
-    protected void playback() {    	
-        //TODO: implement for question 4
+    /**
+     * Playback sequences of recorded notes.
+     */
+    protected void playback() {
+    	if (recordedEvents.isEmpty()) {
+    		return;
+    	} else {
+    		int eventAmount = recordedEvents.size();
+    		if (eventAmount == 1) {
+    			NoteEvent event = recordedEvents.get(0);
+    			executeNoteEvent(event);
+    		} else {
+    			long[] delayTimeArray = new long[eventAmount-1];
+    			delayTimeArray = getDelayTimeArray(recordedEvents);
+    			for (int i = 0; i < eventAmount-1; i = i + 1) {
+    				// play the all but the last note event
+    				NoteEvent event = recordedEvents.get(i);
+    				executeNoteEvent(event);
+    				Midi.wait((int)delayTimeArray[i]);
+    			}
+    			// Don't forget the last note event
+    			NoteEvent lastEvent = recordedEvents.get(eventAmount-1);
+    			executeNoteEvent(lastEvent);
+    		}
+    	}
     }
-
+    
+    /**
+     * Modulate the rawPitch to an appropriate modulatedPitch according to the
+     * piano machine's pitch level (in octaves).
+     * @param rawPitch standard pitch of a musical note
+     * @return modulatedPitch the resulting pitch after modulation
+     */
+    private Pitch modulatePitch(Pitch rawPitch) {
+    	Pitch modulatedPitch = rawPitch;
+    	switch (pitchLevel) {
+    	case DEFAULT_LEVEL:
+    		modulatedPitch = rawPitch;
+    		break;
+    	case ONE_OCTAVE_BELOW:
+    		modulatedPitch = rawPitch.transpose((Pitch.OCTAVE) * (-1));
+    		break;
+    	case TWO_OCTAVES_BELOW:
+    		modulatedPitch =
+    			rawPitch.transpose((Pitch.OCTAVE) * (-1)).transpose((Pitch.OCTAVE) * (-1));
+    		break;
+    	case ONE_OCTAVE_ABOVE:
+    		modulatedPitch = rawPitch.transpose(Pitch.OCTAVE);
+    		break;
+    	case TWO_OCTAVES_ABOVE:
+    		modulatedPitch =
+    			rawPitch.transpose(Pitch.OCTAVE).transpose(Pitch.OCTAVE);
+    		break;
+    	}
+    	return modulatedPitch;
+    }
+    
+    /**
+     * Return the system's current time in millisecond.
+     */
+    private long getCurrentTime() {
+    	Calendar cal = new GregorianCalendar();
+    	return cal.getTimeInMillis();
+    }
+    
+    /**
+     * Play a note, invoked by playback
+     */
+    protected void executeNoteEvent(NoteEvent event) {
+    	if (event.getKind() == NoteEvent.Kind.start) {
+			beginNote(event.getPitch());
+		} else {
+			endNote(event.getPitch());
+		}
+    }
+    
+    /**
+     * Returns an array of delay time between note events.
+     */
+    protected long[] getDelayTimeArray(List<NoteEvent> eventList) {
+    	int eventAmount = eventList.size();
+    	long[] delayTimeArray = new long[eventAmount-1];
+    	for (int i = 0; i < eventAmount-1; i = i + 1) {
+    		long elapsedTime =
+    			(long) Math.floor((double)(eventList.get(i+1).getTime() - eventList.get(i).getTime()) / 10.0);
+    		delayTimeArray[i] = elapsedTime;
+    	}
+    	return delayTimeArray;
+    }
 }
--- Assignments/Problem_Set-2/ps2/src/piano/PianoMachineTest.java
+++ Assignments/Problem_Set-2/ps2/src/piano/PianoMachineTest.java
@@ -1,40 +1,43 @@
 package piano;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
 
 import javax.sound.midi.MidiUnavailableException;
 
+import midi.Instrument;
 import midi.Midi;
+import music.NoteEvent;
 import music.Pitch;
 
 import org.junit.Test;
 
 public class PianoMachineTest {
 	
-	PianoMachine pm = new PianoMachine();
-	
     @Test
     public void singleNoteTest() throws MidiUnavailableException {
-        String expected0 = "on(61,PIANO) wait(100) off(61,PIANO)";
+        String expected = "on(61,PIANO) wait(100) off(61,PIANO)";
+        
+        PianoMachine pm = new PianoMachine();
         
     	Midi midi = Midi.getInstance();
-
     	midi.clearHistory();
     	
         pm.beginNote(new Pitch(1));
 		Midi.wait(100);
 		pm.endNote(new Pitch(1));
-
-        System.out.println(midi.history());
-        assertEquals(expected0, midi.history());
+		
+        assertEquals(expected, midi.history());
     }
     
     @Test
     public void singleRepeatedNoteTest() throws MidiUnavailableException {
-        String expected0 = "on(61,PIANO) wait(0) on(61,PIANO) wait(100) off(61,PIANO)";
+        String expected = "on(61,PIANO) wait(0) on(61,PIANO) wait(100) off(61,PIANO)";
+        
+        PianoMachine pm = new PianoMachine();
         
     	Midi midi = Midi.getInstance();
-
     	midi.clearHistory();
     	
         pm.beginNote(new Pitch(1));
@@ -42,18 +45,18 @@ public class PianoMachineTest {
         pm.beginNote(new Pitch(1));
 		Midi.wait(100);
 		pm.endNote(new Pitch(1));
-
-        System.out.println(midi.history());
-        assertEquals(expected0, midi.history());
+		
+        assertEquals(expected, midi.history());
     }
     
     @Test
     public void twoNotesTest() throws MidiUnavailableException {
-    	String expected1 =
+    	String expected =
     		"on(71,PIANO) wait(0) on(65,PIANO) wait(100) off(65,PIANO) wait(0) off(71,PIANO)";
     	
-    	Midi midi = Midi.getInstance();
+    	PianoMachine pm = new PianoMachine();
     	
+    	Midi midi = Midi.getInstance();
     	midi.clearHistory();
     	
     	pm.beginNote(new Pitch(11));
@@ -64,8 +67,7 @@ public class PianoMachineTest {
     	Midi.wait(0);
     	pm.endNote(new Pitch(11));
     	
-    	System.out.println(midi.history());
-    	assertEquals(expected1, midi.history());
+    	assertEquals(expected, midi.history());
     }
     
     @Test
@@ -73,8 +75,9 @@ public class PianoMachineTest {
     	String expected1 = "on(71,PIANO) wait(0) on(65,PIANO) wait(0) on(71,PIANO) "+
     			"wait(0) on(65,PIANO) wait(100) off(65,PIANO) wait(0) off(71,PIANO)";
     	
-    	Midi midi = Midi.getInstance();
+    	PianoMachine pm = new PianoMachine();
     	
+    	Midi midi = Midi.getInstance();
     	midi.clearHistory();
     	
     	pm.beginNote(new Pitch(11));
@@ -89,7 +92,6 @@ public class PianoMachineTest {
     	Midi.wait(0);
     	pm.endNote(new Pitch(11));
     	
-    	System.out.println(midi.history());
     	assertEquals(expected1, midi.history());
     }
     
@@ -97,22 +99,25 @@ public class PianoMachineTest {
     public void switchToBrightPianoTest() throws MidiUnavailableException {
     	String expected2 = "on(61,BRIGHT_PIANO) wait(100) off(61,BRIGHT_PIANO)";
     	
+    	PianoMachine pm = new PianoMachine();
+    	
     	Midi midi = Midi.getInstance();
+    	midi.clearHistory();
     	
         pm.changeInstrument();
-        midi.clearHistory();
         
         pm.beginNote(new Pitch(1));
 		Midi.wait(100);
 		pm.endNote(new Pitch(1));
 		
-		System.out.println(midi.history());
         assertEquals(expected2, midi.history());
     }
     
     @Test
     public void switchToElectricGrandTest() throws MidiUnavailableException {
-    	String expected3 = "on(61,ELECTRIC_GRAND) wait(100) off(61,ELECTRIC_GRAND)";
+    	String expected = "on(61,ELECTRIC_GRAND) wait(100) off(61,ELECTRIC_GRAND)";
+    	
+    	PianoMachine pm = new PianoMachine();
     	
     	Midi midi = Midi.getInstance();
     	
@@ -124,13 +129,14 @@ public class PianoMachineTest {
 		Midi.wait(100);
 		pm.endNote(new Pitch(1));
 		
-		System.out.println(midi.history());
-        assertEquals(expected3, midi.history());
+        assertEquals(expected, midi.history());
     }
     
     @Test
     public void switchToMusicBoxTest() throws MidiUnavailableException {
-    	String expected4 = "on(61,MUSIC_BOX) wait(100) off(61,MUSIC_BOX)";
+    	String expected = "on(61,MUSIC_BOX) wait(100) off(61,MUSIC_BOX)";
+    	
+    	PianoMachine pm = new PianoMachine();
     	
     	Midi midi = Midi.getInstance();
     	
@@ -154,14 +160,15 @@ public class PianoMachineTest {
 		Midi.wait(100);
 		pm.endNote(new Pitch(1));
 		
-		System.out.println(midi.history());
-        assertEquals(expected4, midi.history());
+        assertEquals(expected, midi.history());
     }
     
     @Test
     public void shiftUpByOneOctaveTest() throws MidiUnavailableException {
     	String expected0 = "on(72,PIANO) wait(100) off(72,PIANO)";
     	String expected1 = "on(83,PIANO) wait(100) off(83,PIANO)";
+    	
+    	PianoMachine pm = new PianoMachine();
     	
     	Midi midi = Midi.getInstance();
     	
@@ -172,7 +179,6 @@ public class PianoMachineTest {
 		Midi.wait(100);
 		pm.endNote(new Pitch(0));
     	
-		System.out.println(midi.history());
         assertEquals(expected0, midi.history());
         
         midi.clearHistory();
@@ -181,7 +187,6 @@ public class PianoMachineTest {
 		Midi.wait(100);
 		pm.endNote(new Pitch(11));
 		
-		System.out.println(midi.history());
         assertEquals(expected1, midi.history());
     }
     
@@ -190,6 +195,8 @@ public class PianoMachineTest {
     	String expected0 = "on(84,PIANO) wait(100) off(84,PIANO)";
     	String expected1 = "on(95,PIANO) wait(100) off(95,PIANO)";
     	
+    	PianoMachine pm = new PianoMachine();
+    	
     	Midi midi = Midi.getInstance();
     	
     	pm.shiftUp();
@@ -200,7 +207,6 @@ public class PianoMachineTest {
 		Midi.wait(100);
 		pm.endNote(new Pitch(0));
     	
-		System.out.println(midi.history());
         assertEquals(expected0, midi.history());
         
         midi.clearHistory();
@@ -209,7 +215,6 @@ public class PianoMachineTest {
 		Midi.wait(100);
 		pm.endNote(new Pitch(11));
 		
-		System.out.println(midi.history());
         assertEquals(expected1, midi.history());;
     }
     
@@ -218,6 +223,8 @@ public class PianoMachineTest {
     	String expected0 = "on(84,PIANO) wait(100) off(84,PIANO)";
     	String expected1 = "on(95,PIANO) wait(100) off(95,PIANO)";
     	
+    	PianoMachine pm = new PianoMachine();
+    	
     	Midi midi = Midi.getInstance();
     	
     	pm.shiftUp();
@@ -229,7 +236,6 @@ public class PianoMachineTest {
 		Midi.wait(100);
 		pm.endNote(new Pitch(0));
     	
-		System.out.println(midi.history());
         assertEquals(expected0, midi.history());
         
         midi.clearHistory();
@@ -238,7 +244,6 @@ public class PianoMachineTest {
 		Midi.wait(100);
 		pm.endNote(new Pitch(11));
 		
-		System.out.println(midi.history());
         assertEquals(expected1, midi.history());;
     }
     
@@ -247,6 +252,8 @@ public class PianoMachineTest {
     	String expected0 = "on(48,PIANO) wait(100) off(48,PIANO)";
     	String expected1 = "on(59,PIANO) wait(100) off(59,PIANO)";
     	
+    	PianoMachine pm = new PianoMachine();
+    	
     	Midi midi = Midi.getInstance();
     	
     	pm.shiftDown();
@@ -256,7 +263,6 @@ public class PianoMachineTest {
 		Midi.wait(100);
 		pm.endNote(new Pitch(0));
     	
-		System.out.println(midi.history());
         assertEquals(expected0, midi.history());
         
         midi.clearHistory();
@@ -265,7 +271,6 @@ public class PianoMachineTest {
 		Midi.wait(100);
 		pm.endNote(new Pitch(11));
 		
-		System.out.println(midi.history());
         assertEquals(expected1, midi.history());
     }
     
@@ -274,6 +279,8 @@ public class PianoMachineTest {
     	String expected0 = "on(36,PIANO) wait(100) off(36,PIANO)";
     	String expected1 = "on(47,PIANO) wait(100) off(47,PIANO)";
     	
+    	PianoMachine pm = new PianoMachine();
+    	
     	Midi midi = Midi.getInstance();
     	
     	pm.shiftDown();
@@ -284,7 +291,6 @@ public class PianoMachineTest {
 		Midi.wait(100);
 		pm.endNote(new Pitch(0));
     	
-		System.out.println(midi.history());
         assertEquals(expected0, midi.history());
         
         midi.clearHistory();
@@ -293,7 +299,6 @@ public class PianoMachineTest {
 		Midi.wait(100);
 		pm.endNote(new Pitch(11));
 		
-		System.out.println(midi.history());
         assertEquals(expected1, midi.history());;
     }
     
@@ -302,6 +307,8 @@ public class PianoMachineTest {
     	String expected0 = "on(36,PIANO) wait(100) off(36,PIANO)";
     	String expected1 = "on(47,PIANO) wait(100) off(47,PIANO)";
     	
+    	PianoMachine pm = new PianoMachine();
+    	
     	Midi midi = Midi.getInstance();
     	
     	pm.shiftDown();
@@ -313,7 +320,6 @@ public class PianoMachineTest {
 		Midi.wait(100);
 		pm.endNote(new Pitch(0));
     	
-		System.out.println(midi.history());
         assertEquals(expected0, midi.history());
         
         midi.clearHistory();
@@ -322,7 +328,317 @@ public class PianoMachineTest {
 		Midi.wait(100);
 		pm.endNote(new Pitch(11));
 		
-		System.out.println(midi.history());
         assertEquals(expected1, midi.history());;
+    }
+    
+    @Test
+    public void recordNoneTest() throws MidiUnavailableException {
+    	
+    	PianoMachine pm = new PianoMachine();
+    	
+    	Midi midi = Midi.getInstance();
+    	midi.clearHistory();
+    	
+    	assertFalse(pm.isRecording);
+    	
+    	pm.toggleRecording();
+    	assertTrue(pm.isRecording);
+    	pm.toggleRecording();
+    	assertFalse(pm.isRecording);
+    	
+    	pm.beginNote(new Pitch(0));
+		Midi.wait(100);
+		pm.endNote(new Pitch(0));
+		
+		assertEquals(pm.recordedEvents, new ArrayList<NoteEvent>());
+    }
+    
+    @Test
+    public void recordOneNoteEventTest() throws MidiUnavailableException {
+    	
+    	PianoMachine pm = new PianoMachine();
+    	
+    	Pitch expectedPitch = new Pitch(0);
+    	Instrument expectedInstrument = Instrument.PIANO;
+    	NoteEvent.Kind expectedBeginNote = NoteEvent.Kind.start;
+    	NoteEvent.Kind expectedEndNote = NoteEvent.Kind.stop;
+    	
+    	Midi midi = Midi.getInstance();
+    	midi.clearHistory();
+    	
+    	assertFalse(pm.isRecording);
+    	
+    	pm.toggleRecording();
+    	assertTrue(pm.isRecording);
+    	
+    	pm.beginNote(new Pitch(0));
+		Midi.wait(100);
+		pm.endNote(new Pitch(0));
+		
+		pm.toggleRecording();
+    	assertFalse(pm.isRecording);
+		
+    	assertEquals(pm.recordedEvents.get(0).getPitch(), expectedPitch);
+		assertEquals(pm.recordedEvents.get(0).getInstr(), expectedInstrument);
+		assertEquals(pm.recordedEvents.get(0).getKind(), expectedBeginNote);
+		
+		assertEquals(pm.recordedEvents.get(1).getPitch(), expectedPitch);
+		assertEquals(pm.recordedEvents.get(1).getInstr(), expectedInstrument);
+		assertEquals(pm.recordedEvents.get(1).getKind(), expectedEndNote);
+    }
+    
+    @Test
+    public void recordThreeNoteEventsTest() throws MidiUnavailableException {
+    	
+    	PianoMachine pm = new PianoMachine();
+    	
+    	Pitch expectedPitch0 = new Pitch(0);
+    	Pitch expectedPitch5 = new Pitch(5);
+    	Pitch expectedPitch8 = new Pitch(8);
+    	Instrument expectedInstrument = Instrument.PIANO;
+    	NoteEvent.Kind expectedBeginNote = NoteEvent.Kind.start;
+    	NoteEvent.Kind expectedEndNote = NoteEvent.Kind.stop;
+    	
+    	Midi midi = Midi.getInstance();
+    	midi.clearHistory();
+    	
+    	assertFalse(pm.isRecording);
+    	
+    	pm.toggleRecording();
+    	assertTrue(pm.isRecording);
+    	
+    	// the first note event
+    	pm.beginNote(new Pitch(0));
+		Midi.wait(100);
+		pm.endNote(new Pitch(0));
+		
+		// the first note event
+		Midi.wait(10);
+		pm.beginNote(new Pitch(5));
+		Midi.wait(100);
+		pm.endNote(new Pitch(5));
+		
+		// the third note event
+		Midi.wait(10);
+		pm.beginNote(new Pitch(8));
+		Midi.wait(100);
+		pm.endNote(new Pitch(8));
+		
+		pm.toggleRecording();
+    	assertFalse(pm.isRecording);
+		
+    	assertEquals(pm.recordedEvents.get(0).getPitch(), expectedPitch0);
+		assertEquals(pm.recordedEvents.get(0).getInstr(), expectedInstrument);
+		assertEquals(pm.recordedEvents.get(0).getKind(), expectedBeginNote);
+		
+		assertEquals(pm.recordedEvents.get(1).getPitch(), expectedPitch0);
+		assertEquals(pm.recordedEvents.get(1).getInstr(), expectedInstrument);
+		assertEquals(pm.recordedEvents.get(1).getKind(), expectedEndNote);
+		
+		assertEquals(pm.recordedEvents.get(2).getPitch(), expectedPitch5);
+		assertEquals(pm.recordedEvents.get(2).getInstr(), expectedInstrument);
+		assertEquals(pm.recordedEvents.get(2).getKind(), expectedBeginNote);
+		
+		assertEquals(pm.recordedEvents.get(3).getPitch(), expectedPitch5);
+		assertEquals(pm.recordedEvents.get(3).getInstr(), expectedInstrument);
+		assertEquals(pm.recordedEvents.get(3).getKind(), expectedEndNote);
+		
+		assertEquals(pm.recordedEvents.get(4).getPitch(), expectedPitch8);
+		assertEquals(pm.recordedEvents.get(4).getInstr(), expectedInstrument);
+		assertEquals(pm.recordedEvents.get(4).getKind(), expectedBeginNote);
+		
+		assertEquals(pm.recordedEvents.get(5).getPitch(), expectedPitch8);
+		assertEquals(pm.recordedEvents.get(5).getInstr(), expectedInstrument);
+		assertEquals(pm.recordedEvents.get(5).getKind(), expectedEndNote);
+    }
+    
+    @Test
+    public void overrideRecordTest() throws MidiUnavailableException {
+    	PianoMachine pm = new PianoMachine();
+    	
+    	Pitch expectedPitch0 = new Pitch(0);
+    	Pitch expectedPitch2 = new Pitch(2);
+    	Pitch expectedPitch5 = new Pitch(5);
+    	Pitch expectedPitch7 = new Pitch(7);
+    	Pitch expectedPitch10 = new Pitch(10);
+    	Instrument expectedInstrument = Instrument.PIANO;
+    	NoteEvent.Kind expectedBeginNote = NoteEvent.Kind.start;
+    	NoteEvent.Kind expectedEndNote = NoteEvent.Kind.stop;
+    	
+    	Midi midi = Midi.getInstance();
+    	midi.clearHistory();
+    	
+    	assertFalse(pm.isRecording);
+    	
+    	// the initial recording
+    	pm.toggleRecording();
+    	assertTrue(pm.isRecording);
+    	
+    	// the first note event
+    	pm.beginNote(new Pitch(0));
+		Midi.wait(100);
+		pm.endNote(new Pitch(0));
+		
+		// the second note event
+		Midi.wait(10);
+		pm.beginNote(new Pitch(5));
+		Midi.wait(100);
+		pm.endNote(new Pitch(5));
+		
+		pm.toggleRecording();
+    	assertFalse(pm.isRecording);
+		
+    	assertEquals(pm.recordedEvents.get(0).getPitch(), expectedPitch0);
+		assertEquals(pm.recordedEvents.get(0).getInstr(), expectedInstrument);
+		assertEquals(pm.recordedEvents.get(0).getKind(), expectedBeginNote);
+		
+		assertEquals(pm.recordedEvents.get(1).getPitch(), expectedPitch0);
+		assertEquals(pm.recordedEvents.get(1).getInstr(), expectedInstrument);
+		assertEquals(pm.recordedEvents.get(1).getKind(), expectedEndNote);
+		
+		assertEquals(pm.recordedEvents.get(2).getPitch(), expectedPitch5);
+		assertEquals(pm.recordedEvents.get(2).getInstr(), expectedInstrument);
+		assertEquals(pm.recordedEvents.get(2).getKind(), expectedBeginNote);
+		
+		assertEquals(pm.recordedEvents.get(3).getPitch(), expectedPitch5);
+		assertEquals(pm.recordedEvents.get(3).getInstr(), expectedInstrument);
+		assertEquals(pm.recordedEvents.get(3).getKind(), expectedEndNote);
+		
+		// the new recording, expected to override the previous one
+    	pm.toggleRecording();
+    	assertTrue(pm.isRecording);
+    	
+    	// the first note event
+    	pm.beginNote(new Pitch(2));
+		Midi.wait(100);
+		pm.endNote(new Pitch(2));
+		
+		// the second note event
+		Midi.wait(10);
+		pm.beginNote(new Pitch(7));
+		Midi.wait(100);
+		pm.endNote(new Pitch(7));
+		
+		// the third note event
+		Midi.wait(10);
+		pm.beginNote(new Pitch(10));
+		Midi.wait(100);
+		pm.endNote(new Pitch(10));
+		
+		pm.toggleRecording();
+    	assertFalse(pm.isRecording);
+		
+    	assertEquals(pm.recordedEvents.get(0).getPitch(), expectedPitch2);
+		assertEquals(pm.recordedEvents.get(0).getInstr(), expectedInstrument);
+		assertEquals(pm.recordedEvents.get(0).getKind(), expectedBeginNote);
+		
+		assertEquals(pm.recordedEvents.get(1).getPitch(), expectedPitch2);
+		assertEquals(pm.recordedEvents.get(1).getInstr(), expectedInstrument);
+		assertEquals(pm.recordedEvents.get(1).getKind(), expectedEndNote);
+		
+		assertEquals(pm.recordedEvents.get(2).getPitch(), expectedPitch7);
+		assertEquals(pm.recordedEvents.get(2).getInstr(), expectedInstrument);
+		assertEquals(pm.recordedEvents.get(2).getKind(), expectedBeginNote);
+		
+		assertEquals(pm.recordedEvents.get(3).getPitch(), expectedPitch7);
+		assertEquals(pm.recordedEvents.get(3).getInstr(), expectedInstrument);
+		assertEquals(pm.recordedEvents.get(3).getKind(), expectedEndNote);
+		
+		assertEquals(pm.recordedEvents.get(4).getPitch(), expectedPitch10);
+		assertEquals(pm.recordedEvents.get(4).getInstr(), expectedInstrument);
+		assertEquals(pm.recordedEvents.get(4).getKind(), expectedBeginNote);
+		
+		assertEquals(pm.recordedEvents.get(5).getPitch(), expectedPitch10);
+		assertEquals(pm.recordedEvents.get(5).getInstr(), expectedInstrument);
+		assertEquals(pm.recordedEvents.get(5).getKind(), expectedEndNote);
+    }
+    
+    @Test
+    public void playbackNoRecordingTest () throws MidiUnavailableException {
+    	
+    	String expectedBehavior = "";
+    	
+    	PianoMachine pm = new PianoMachine();
+    	
+    	Midi midi = Midi.getInstance();
+    	midi.clearHistory();
+    	
+    	assertFalse(pm.isRecording);
+    	
+    	pm.toggleRecording();
+    	assertTrue(pm.isRecording);
+    	pm.toggleRecording();
+    	assertFalse(pm.isRecording);
+    	
+    	pm.beginNote(new Pitch(0));
+		Midi.wait(100);
+		pm.endNote(new Pitch(0));
+		
+		midi.clearHistory();
+		pm.playback();
+		assertEquals(expectedBehavior, midi.history());
+    }
+    
+    @Test
+    public void playbackOneNoteEventTest() throws MidiUnavailableException {
+    	
+    	String expectedBehavior = "on(60,PIANO) wait(100) off(60,PIANO)";
+    	
+    	PianoMachine pm = new PianoMachine();
+    	
+    	Midi midi = Midi.getInstance();
+    	midi.clearHistory();
+    	
+    	assertFalse(pm.isRecording);
+    	
+    	pm.toggleRecording();
+    	assertTrue(pm.isRecording);
+    	
+    	pm.beginNote(new Pitch(0));
+		Midi.wait(100);
+		pm.endNote(new Pitch(0));
+		
+		pm.toggleRecording();
+    	assertFalse(pm.isRecording);
+		
+		midi.clearHistory();
+		pm.playback();
+		assertEquals(expectedBehavior, midi.history());
+    }
+    
+    @Test
+    public void playbackThreeNoteEventsTest() throws MidiUnavailableException {
+    	
+    	PianoMachine pm = new PianoMachine();
+    	
+    	String expectedBehavior = "on(60,PIANO) wait(100) off(60,PIANO) wait(20) " +
+    			"on(64,PIANO) wait(100) off(64,PIANO) wait(20) " +
+    			"on(69,PIANO) wait(100) off(69,PIANO)";
+    	
+    	Midi midi = Midi.getInstance();
+    	midi.clearHistory();
+    	
+    	assertFalse(pm.isRecording);
+    	pm.toggleRecording();
+    	assertTrue(pm.isRecording);
+
+        pm.beginNote(new Pitch(0));
+		Midi.wait(100);
+		pm.endNote(new Pitch(0));
+		Midi.wait(20);
+		pm.beginNote(new Pitch(4));
+		Midi.wait(100);
+		pm.endNote(new Pitch(4));
+		Midi.wait(20);
+		pm.beginNote(new Pitch(9));
+		Midi.wait(100);
+		pm.endNote(new Pitch(9));
+		
+		pm.toggleRecording();
+		assertFalse(pm.isRecording);
+		
+		midi.clearHistory();
+		pm.playback();
+		assertEquals(expectedBehavior, midi.history());
     }
 }
