e10706beb841e1afde90d003cf3f04bc38768d5d
Lawrence R. Amlord <informlarry@gmail.com>
2014-10-27 15:09:54 +0800

    add solution to Problem 1 in Problem Set 4.
    The following 2 files are augmented:
    
    	src/sudoku/Sudoku.java
    	src/sudoku/SudokuTest.java
    
    Additionally, the following 3 files are included:
    
    	samples/sudoku_4x4_empty.txt
    	samples/sudoku_9x9_empty.txt
    	src/utils/FileUtils.java
    
    Thank you!
    							Lawrence X. Amlord
    							  Oct. 27th, 2014
    							   Xi'an, China

--- Assignments/Problem_Set-4/ps4/src/sudoku/Sudoku.java
+++ Assignments/Problem_Set-4/ps4/src/sudoku/Sudoku.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import sat.env.Environment;
 import sat.env.Variable;
 import sat.formula.Formula;
+import utils.FileUtils;
 
 /**
  * Sudoku is an immutable abstract datatype representing instances of Sudoku.
@@ -23,20 +24,61 @@ public class Sudoku {
     private final int size;
     // known values: square[i][j] represents the square in the ith row and jth
     // column,
-    // contains -1 if the digit is not present, else i>=0 to represent the digit
-    // i+1
-    // (digits are indexed from 0 and not 1 so that we can take the number k
-    // from square[i][j] and
-    // use it to index into occupies[i][j][k])
+    // contains 0 if the digit is not present, else k (1 <= k <= size) to represent
+    // the digit k
     private final int[][] square;
-    // occupies [i,j,k] means that kth symbol occupies entry in row i, column j
+    // occupies [i,j,k] means that kth (1 <= k <= size) symbol occupies entry in
+    // row i, column j
     private final Variable[][][] occupies;
 
-    // Rep invariant
-    // TODO: write your rep invariant here
+    // Rep invariants:
+    //		(1) dim*dim == size
+    //			the number of rows and columns are: dim*dim
+    //		(2) 2 <= dim <= 3
+    //			the puzzle has at least dim of 2 and at most 3
+    //		(3) 0 <= square[i][j] <= 9
+    //			the puzzle is either filled with digits 1-9 or
+    //			left blank
+    //
+    // Abstract Function:
+    //		The slot square[i][j] represents the ith row and jth column in the
+    //		sudoku puzzle,
+    //			square[i][j] == 0 if the slot is empty, else
+    //			square[i][j] == k (1 <= k <= 9) to represent the digit k
+    //
+    //		For example, if the sudoku puzzle contains
+    //
+    //				+---+---+---+---+
+    //				|   |   |   |   |
+    //				+---+---+---+---+
+    //				| 3 |   |   |   |
+    //				+---+---+---+---+
+    //				|   |   | 4 |   |
+    //				+---+---+---+---+
+    //				|   | 2 | 3 |   |
+    //				+---+---+---+---+
+    //
+    //		then, the corresponding two dimensional array is
+    //
+    //			  { { 0, 0, 0, 0 },
+    //			    { 3, 0, 0, 0 },
+    //			    { 0, 0, 4, 0 },
+    //			    { 0, 2, 3, 0 } }
+    //		
     private void checkRep() {
-        // TODO: implement this.
-        throw new RuntimeException("not yet implemented.");
+    	assert (this.dim * this.dim) == size :
+    		"SudokuPuzzle, Rep invariant: the number of rows and columns "
+    		+ "are: dim*dim";
+    	assert ((this.dim >= 2) && (this.dim <= 3)) :
+    		"SudokuPuzzle, Rep invariant: the puzzle has at least dim of 2 "
+    		+ "and at most dim of 3";
+    	for (int i = 0; i < size; i = i + 1) {
+    		for (int j = 0; j < size; j = j + 1) {
+    			assert ((square[i][j] >= 0) && (square[i][j] <= 9)) :
+    	    		"SudokuPuzzle, Rep invariant: the slots are either filled "
+    	    		+ "with digits 1-9 or left blank";
+    		}
+    	}
     }
 
     /**
@@ -47,8 +89,17 @@ public class Sudoku {
      *            makes a standard Sudoku puzzle with a 9x9 grid.
      */
     public Sudoku(int dim) {
-        // TODO: implement this.
-        throw new RuntimeException("not yet implemented.");
+    	this.dim = dim;
+    	size = dim * dim;
+        int[][] sq = new int[size][size];
+        for (int i = 0; i < size; i = i + 1) {
+        	for (int j = 0; j < size; j = j + 1) {
+        		sq[i][j] = 0;
+        	}
+        }
+        this.square = sq;
+        this.occupies = initializeVariables(size);
+        checkRep();
     }
 
     /**
@@ -71,8 +122,26 @@ public class Sudoku {
      *            square[i].length for 0<=i<(dim^2 - 1).
      */
     public Sudoku(int dim, int[][] square) {
-        // TODO: implement this.
-        throw new RuntimeException("not yet implemented.");
+        this.dim = dim;
+        size = dim * dim;
+        this.square = square;
+        this.occupies = initializeVariables(size);
+        checkRep();
+    }
+    
+    // initialize the occupies[i][j][k] variables in Sudoku's representation
+    private Variable[][][] initializeVariables(int size) {
+    	Variable[][][] vars = new Variable[size][size][size];
+    	for (int i = 0; i < size; i = i + 1) {
+        	for (int j = 0; j < size; j = j + 1) {
+        		for (int k = 0; k < size; k = k + 1) {
+        			Variable var = new Variable(
+        					"occupies(" + i + "," + j + "," + k + ")");
+        			vars[i][j][k] = var;
+        		}
+        	}
+        }
+    	return vars;
     }
 
     /**
@@ -95,8 +164,40 @@ public class Sudoku {
      */
     public static Sudoku fromFile(int dim, String filename) throws IOException,
             ParseException {
-        // TODO: implement this.
-        throw new RuntimeException("not yet implemented.");
+    	String fileContents = FileUtils.fetch(filename);
+    	int size = dim * dim;
+    	char[][] charMatrix = convertStringToCharMatrix(fileContents);
+    	int[][] digitMatrix = new int[size][size];
+    	for (int i = 0; i < size; i = i + 1) {
+    		for (int j = 0; j < size; j = j + 1) {
+    			if (charMatrix[i][j] == '.') {
+    				digitMatrix[i][j] = 0;
+    			} else {
+    				assert (Character.getNumericValue(charMatrix[i][j]) >= 1) &&
+    					(Character.getNumericValue(charMatrix[i][j]) <= 9) :
+    						"fromFile: only digits 1-9, '.' and '\\n' are legal"
+    						+ " in the text representation of Sudoku puzzle.";
+    				digitMatrix[i][j] = Character.getNumericValue(charMatrix[i][j]);
+    			}
+    		}
+    	}
+    	return new Sudoku(dim, digitMatrix);
+    }
+    
+    // convert a string into a matrix of characters
+    // each row in the matrix corresponds to a substring partitioned
+    // by the character '\n'
+    private static char[][] convertStringToCharMatrix(String text) {
+    	String[] lines = text.split("\\n");
+    	int size = lines.length;
+    	// System.out.println("lines: " + size);
+    	char[][] charMatrix = new char[size][size];
+    	for (int i = 0; i < size; i = i + 1) {
+    		for (int j = 0; j < size; j = j + 1) {
+    			charMatrix[i][j] = lines[i].charAt(j);
+    		}
+    	}
+    	return charMatrix;
     }
 
     /**
@@ -110,7 +211,7 @@ public class Sudoku {
     }
 
     /**
-     * Produce readable string representation of this Sukoku grid, e.g. for a 4
+     * Produce readable string representation of this sudoku grid, e.g. for a 4
      * x 4 sudoku problem: 
      *   12.4 
      *   3412 
@@ -120,8 +221,19 @@ public class Sudoku {
      * @return a string corresponding to this grid
      */
     public String toString() {
-        // TODO: implement this.
-        throw new RuntimeException("not yet implemented.");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < size; i = i + 1) {
+        	for (int j = 0; j < size; j = j + 1) {
+        		if (square[i][j] == 0) {
+        			sb.append('.');
+        		} else {
+        			checkRep(); // assert that 1 <= square[i][j] <= 9
+        			sb.append(square[i][j]);
+        		}
+        	}
+        	sb.append('\n');
+        }
+        return sb.toString();
     }
 
     /**
